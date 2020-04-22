9fe7d53fadde8c4bad39331099bc40636bcb1239
Salar Soleimani <s.s_m1983@yahoo.com>
2020-04-21 21:11:14 -0400

    Initial Commit

---
+++ Clean Starter Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clean Starter Project
+//
+//  Created by Salar Soleimani on 2020-04-21.
+//  Copyright © 2020 BeKSaS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
